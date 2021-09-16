a60db3dbd2a24dbebf7c652976164a036fc968e6
Juliany Bandeira <juliany12121989@gmail.com>
2021-09-16 11:06:22 -0300

    lista produtos

--- src/components/cardViagem.js
+++ src/components/cardViagem.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const ViagemContainer = styled.div`
+    
+`
+
+const ViagemFoto = styled.img`
+    width: 300px;
+`
+
+const ViagemTexto= styled.p``
 
 
-export default class CardViagem extends React.Component {
 
-    render() {
-        return (
-            <div> 
-                {this.props.imagem}
-                {this.props.texto}
-                {this.props.valor}
-            </div> 
+export default function CardViagem (props) {
+    return (
+        <ViagemContainer> 
+            <ViagemFoto src={props.produto.imagem} alt={'Paisagem do destino da viagem'}/>
+            <p>{props.produto.texto}</p>
+            <p>{props.produto.valor.toFixed(2)}</p>
+            <button>Adicionar ao carrinho</button>
+        </ViagemContainer> 
         )
     };
-}
